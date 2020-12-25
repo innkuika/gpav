6aa9804bcb2bf4a9c4131e2c36f220fd25477a39
KTachibanaM <whj19931115@gmail.com>
2020-12-24 18:07:21 -0800

    settings for show private posts

---
+++ gpav_app/settings.py
@@ -0,0 +1,3 @@
+import os
+
+SHOW_PRIVATE_POSTS = os.environ.get('SHOW_PRIVATE_POSTS', 'false') == 'true'
--- gpav_app/views.py
+++ gpav_app/views.py
@@ -1,10 +1,13 @@
 from .models import Post
-from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.shortcuts import render, get_object_or_404
 from django.views.generic.list import ListView
+from django.http import Http404
+from .settings import SHOW_PRIVATE_POSTS
 
 
 class PostListView(ListView):
-    model = Post
+    # TODO: audience_html__contains='Public' duplicates with is_public
+    queryset = Post.objects.all() if SHOW_PRIVATE_POSTS else Post.objects.filter(audience_html__contains='Public')
     paginate_by = 25
     ordering = '-date_created'
     template_name = 'index.html'
@@ -12,9 +15,10 @@ class PostListView(ListView):
 
 def post(request, post_id):
     p = get_object_or_404(Post, id=post_id)
+    if not SHOW_PRIVATE_POSTS and not p.is_public():
+        raise Http404()
     context = {
         'post': p,
-        'comments': p.comments.all(),
         'plus_oners': ', '.join(map(lambda pp: pp.name, p.plus_oners.all())),
         'resharers': ', '.join(map(lambda pp: pp.name, p.resharers.all()))
     }
